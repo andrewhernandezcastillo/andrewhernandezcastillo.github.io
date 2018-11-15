82533ff011a4548b944d38b0f922312768283a75
andrewhernandezcastillo <45079384+andrewhernandezcastillo@users.noreply.github.com>
2018-11-15 16:25:05 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+<title>UAH</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+
+<div class="bgded overlay light" style="background-image:url('images/demo/backgrounds/UAH.png');"> 
+  
+  <div class="wrapper row0">
+    <div id="topbar" class="hoc clear"> 
+      
+      <div class="fl_left"> 
+        
+        <ul class="nospace">
+          <li><i class="fa fa-phone"></i> +58 (212) 418 5131</li>
+          <li><i class="fa fa-envelope-o"></i> UAHumboldt.com</li>
+          <li><i class="fa fa-envelope-o"></i> Alejandrodehumboldt@gmail.com </li>
+        </ul>
+       
+      </div>
+      <div class="fl_right"> 
+        
+        <ul class="nospace">
+          <li><a href="index.php"><i class="fa fa-lg fa-home"></i></a></li>
+          <li><a href="#" title="Ayuda Centre"><i class="fa fa-life-bouy"></i></a></li>
+          <li><a href="index.php" title="Login"><i class="fa fa-lg fa-sign-in"></i></a></li>
+          <li><a href="index.php" title="Iniciar sesion"><i class="fa fa-lg fa-edit"></i></a></li>
+        </ul>
+
+      </div>
+
+    </div>
+  </div>
+
+  <div class="wrapper row1">
+    <header id="header" class="hoc clear"> 
+
+      <div id="logo" class="fl_left">
+        <h1><a href="index.php">Inicio</a></h1>
+      </div>
+      <nav id="mainav" class="fl_right">
+        <ul class="clear">
+          <li class="active"><a href="index.php">Inicio</a></li>
+          <li><a class="drop" href="#">Paginas</a>
+            <ul>
+              <li><a href="pages/Galeria.php">Galeria</a></li>
+              <li><a href="pages/Historia.php">Historia</a></li>
+            </ul>
+          </li>
+          <li><a href="index.php">salir</a></li>
+        </ul>
+      </nav>
+
+    </header>
+  </div>
+
+  <div id="pageintro" class="hoc clear"> 
+
+    <article>
+      <h3 class="heading">UAH Universidad Alejandro de Humboldt<br>
+        
+      <p> ¡ Unete a nuestra comunidad y enterate de todo !.</p>
+      <footer><a class="btn" href="#">Registro</a></footer>
+    </article>
+
+  </div>
+
+</div>
+
+
+<div class="wrapper row3">
+  <main class="hoc container clear"> 
+
+    <div class="group excerpt">
+      <article class="one_third first"><a class="imgover btmspace-30" href="#"><img src="images/demo/320x240.jpg" alt=""></a>
+        <h6 class="heading">Imagenes referentes a la tematica</h6>
+        <p>Descripcion que se quiera dar referente a la imagen&hellip;</p>
+        <footer><a href="#">Ver mas &raquo;</a></footer>
+      </article>
+      <article class="one_third"><a class="imgover btmspace-30" href="#"><img src="images/demo/320x240.jpg" alt=""></a>
+        <h6 class="heading">Imagenes referentes a la tematica</h6>
+        <p>Descripcion que se quiera dar referente a la imageno&hellip;</p>
+        <footer><a href="#">Ver mas &raquo;</a></footer>
+      </article>
+      <article class="one_third"><a class="imgover btmspace-30" href="#"><img src="images/demo/320x240.jpg" alt=""></a>
+        <h6 class="heading">Imagenes referentes a la tematica</h6>
+        <p>Descripcion que se quiera dar referente a la imagen&hellip;</p>
+        <footer><a href="#">Ver mas &raquo;</a></footer>
+      </article>
+    </div>
+
+    <div class="clear"></div>
+  </main>
+</div>
+
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+</body>
+</html>
